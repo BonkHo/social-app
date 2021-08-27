ebc0f55ec7e233318eb789a7f21a2ab534d935bb
John Bae <john21225@gmail.com>
2021-08-27 15:40:21 -0700

    Home, Login, and Register page routes added to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// Styles
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+// SUI Components
+import { Container } from "semantic-ui-react";
+// Components
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Menu from "./components/MenuBar";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+	return (
+		<Router>
+			<Container>
+				<Menu />
+				<Route exact path="/" component={Home}></Route>
+				<Route exact path="/login" component={Login}></Route>
+				<Route exact path="/register" component={Register}></Route>
+			</Container>
+		</Router>
+	);
 }
 
 export default App;
---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+// This component was obtained from Semantic UI and is used as the NavBar for every single page
+// Semantic UI is similar to Material UI and Bootstrap
+// Some components in Semantic UI are class components and should be converted into functional components before use
+// Functional components use hooks instead of state variables, dont require render functions, and dont use the reference 'this'
+
+const MenuBar = () => {
+	// Logic to figure out the active path
+	const pathName = window.location.pathname;
+	console.log(pathName);
+	const path = pathName === "/" ? "home" : pathName.substr(1);
+
+	// Sets state to the active path
+	const [activeItem, setActiveItem] = useState(path);
+
+	// Listens for onClick event to setActiveItem
+	const handleItemClick = (e, { name }) => setActiveItem(name);
+
+	return (
+		<Menu pointing secondary size="massive" color="teal">
+			<Menu.Item
+				name="home"
+				active={activeItem === "home"}
+				onClick={handleItemClick}
+				as={Link}
+				to="/"
+			/>
+			<Menu.Menu position="right">
+				<Menu.Item
+					name="login"
+					active={activeItem === "login"}
+					onClick={handleItemClick}
+					as={Link}
+					to="/login"
+				/>
+				<Menu.Item
+					name="register"
+					active={activeItem === "register"}
+					onClick={handleItemClick}
+					as={Link}
+					to="/register"
+				/>
+			</Menu.Menu>
+		</Menu>
+	);
+};
+
+export default MenuBar;
