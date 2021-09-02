2225cce75afb79ca1e511f94bcbd6508ffcfaeba
John Bae <john21225@gmail.com>
2021-09-02 11:59:11 -0700

    Checks user token to preserve user sessions on refresh

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,4 +1,22 @@
+// JSX & Hooks
 import React, { createContext, useReducer } from "react";
+// jwt Token decoder
+import jwtDecode from "jwt-decode";
+
+const initialState = {
+	user: null,
+};
+
+// Checks to see if 'jwtToken' item exists in local storage
+if (localStorage.getItem("jwtToken")) {
+	const decodedToken = jwtDecode(localStorage.getItem("jwtToken"));
+	// Checks to see if the decoded token is expired
+	if (decodedToken.exp * 1000 < Date.now()) {
+		localStorage.removeItem("jwtToken");
+	} else {
+		initialState.user = decodedToken;
+	}
+}
 
 const AuthContext = createContext({
 	user: null,
@@ -26,8 +44,10 @@ function authReducer(state, action) {
 }
 
 function AuthProvider(props) {
-	const [state, dispatch] = useReducer(authReducer, { user: null });
+	const [state, dispatch] = useReducer(authReducer, initialState);
 	function login(userData) {
+		// Stores the user token in local storage
+		localStorage.setItem("jwtToken", userData.token);
 		dispatch({
 			type: "LOGIN",
 			payload: userData,
@@ -35,6 +55,8 @@ function AuthProvider(props) {
 	}
 
 	function logout() {
+		// Removes the user token in local storage
+		localStorage.removeItem("jwtToken");
 		dispatch({
 			type: "LOGOUT",
 		});
